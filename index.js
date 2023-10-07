f19d5f01c76be03135b30a8a2e7d0040d8717331
sougata2002 <sougatabhowmick62@gmail.com>
2023-10-08 04:35:26 +0530

    Css done && empty list handled!!

--- index.js
+++ index.js
@@ -15,17 +15,30 @@ async function myTask(){
     let url=`http://www.omdbapi.com/?s=${inputValue}&apikey=bdb7c0ef`;
     let data=await fetch (url);
     let response = await data.json();
-    console.log(response);
-    show(response.Search);
+    console.log(response.Search);
+    if(response.Search==null){
+        empty();
+    }
+    else{
+        show(response.Search);
+    }
 }
 
 function show(d){
     list.innerHTML="";
         // console.log(d[0]);
         d.forEach(element => {
-        let div=document.createElement("div");
+        let div=document.createElement("section");
         div.classList.add("c");
-        div.innerHTML=`<span style="display:flex;flex-direction:column;justify-content:center;font-size:2rem;"><span style="margin-bottom:20px;"><b>Title: </b>${element.Title}</span><span><b>Year: </b>${element.Year}</span></span><span ><img src="${element.Poster}" width="200px" height="200px"></span>`;
+        div.innerHTML=`<div>
+                         <span">
+                            <b>Title: </b>${element.Title}</span><br/>
+                        <span>
+                              <b>Year: </b>${element.Year}</span>
+                        </div>
+                        <div >
+                        <img class="listImg" src="${element.Poster}">
+                        </div>`;
         list.append(div);
         
        });
@@ -33,4 +46,13 @@ function show(d){
     // div.innerHTML=`<span>${d.Title}</span>`;
     // list.append(div);
     
+
+}
+
+const empty = ()=>{
+    list.innerHTML="";
+    let div=document.createElement("div");
+    div.classList.add("em");
+    div.innerHTML=`<h1>No movies found!!</h1><p>Try with another keyword</p>`;
+    list.append(div);
 }
